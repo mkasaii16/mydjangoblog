a58b97f3f172e402e61c59d5c10095e6c71f42d7
mkasaii16@gmail.com <dayan0156801568dayan>
2021-03-31 21:02:33 +0430

    new edit

--- articles/views.py
+++ articles/views.py
@@ -8,4 +8,6 @@ def articles_list(request):
     return render(request,'articles/articleslist.html',args)
 
 def article_detail(request, slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slug)
+    article = models.Articles.objects.get(slug=slug)
+    return render(request,'articles/article_detail.html',{'article':article})
