488e635dc482990b1c7221cb64527aff86ba41d4
mkasaii16@gmail.com <dayan0156801568dayan>
2021-03-31 19:22:46 +0430

    other ....

--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = "articles"
+
 urlpatterns = [
-    path('',views.articles_list),
+    path('',views.articles_list,name = "list"),
+    path('<slug>',views.article_detail,name="detail"),
 ]
